4aeb4296afaf912950d4b66f96ac43284cb784ee
opiter09 <92452568+opiter09@users.noreply.github.com>
2023-09-18 21:10:27 -0400

    this should be it

--- randomize.py
+++ randomize.py
@@ -44,11 +44,6 @@ if (good == 1):
     vivos = list(range(1, 150))
     random.shuffle(vivos)
     vivos = [0] + vivos
-    donate = [65, 20, 2, 34]
-    for d in donate:
-        x = vivos.index(d)
-        vivos[x] = vivos[d]
-        vivos[d] = d
 
     broken = list(brokenR.replace(" ", "").replace("\n", "").split(","))
     broken = list(set(broken))
@@ -56,11 +51,6 @@ if (good == 1):
         broken = [ max(1, min(149, int(x))) for x in broken ]
     except:
         broken = []
-    for d in donate:
-        try:
-            broken.remove(d)
-        except:
-            pass
 
     shift = [3, 23, 43, 58, 83, 5, 37, 62, 87, 91, 27, 29, 41, 68, 80, 45, 55, 60, 64, 90, 21, 40, 42, 49, 84]
     for b in broken:
@@ -80,6 +70,17 @@ if (good == 1):
         y = vivos[shift[i]]
         vivos[ind] = y
         vivos[shift[i]] = x
+        
+    vivoNames = list(open("ffc_vivoNames.txt", "rt").read().split("\n")[0:149])
+    fossilNames = list(open("ffc_kasekiNames.txt", "rt").read().split("\n"))
+    fossilTable = { "Head": [0], "Body": [0], "Arms": [0], "Legs": [0] }
+    for v in vivoNames:
+        for k in fossilTable.keys():
+            if ((v + " " + k) in fossilNames):
+                fossilTable[k].append(fossilNames.index(v + " " + k))
+            else:
+                fossilTable[k].append(fossilNames.index(v + " Single"))
+    # print(fossilTable["Head"])
 
     try:
         shutil.rmtree("NDS_UNPACK")
@@ -92,95 +93,124 @@ if (good == 1):
         for root, dirs, files in os.walk("NDS_UNPACK/data/map/m/bin"):
             for file in files:
                 if (file == "0.bin"):
+                    used = {}
                     f = open(os.path.join(root, file), "rb")
                     r = f.read()
                     f.close()
                     f = open(os.path.join(root, file), "wb")
                     f.close()
                     f = open(os.path.join(root, file), "ab")
+                    point = int.from_bytes(r[0x6C:0x70], "little")
                     mapN = os.path.join(root, file).split("\\")[-2]
-                    numTables = int.from_bytes(r[0x50:0x54], "little")
-                    point = int.from_bytes(r[0x54:0x58], "little")
-                    realP = []
-                    loc = point
-                    for i in range(numTables):
+                    realP = [ int.from_bytes(r[point:(point + 4)], "little") ]
+                    loc = point + 4
+                    while (realP[-1] > 0):
                         realP.append(int.from_bytes(r[loc:(loc + 4)], "little"))
                         loc = loc + 4
-                    realP.append(len(r))
-                    f.write(r[0:realP[0]])
-                    for val in realP[0:-1]:
-                        index = int.from_bytes(r[(val + 4):(val + 8)], "little")
+                    realP = realP[0:-1]
+                    for val in realP:
+                        index = int.from_bytes(r[(val + 2):(val + 4)], "little")
                         if (index == 0):
-                            f.write(r[val:realP[realP.index(val) + 1]])
                             continue
+                        numTables = int.from_bytes(r[(val + 12):(val + 16)], "little")
+                        point3 = int.from_bytes(r[(val + 16):(val + 20)], "little")
+                        for i in range(numTables):
+                            point4 = int.from_bytes(r[(val + point3 + (i * 4)):(val + point3 + (i * 4) + 4)], "little")
+                            point5 = int.from_bytes(r[(val + point4 + 12):(val + point4 + 16)], "little")
+                            numWeird = int.from_bytes(r[(val + point4 + point5 + 8):(val + point4 + point5 + 12)], "little")
+                            numSpawns = int.from_bytes(r[(val + point4 + point5 + 16):(val + point4 + point5 + 20)], "little")
+                            startSpawns = val + point4 + point5 + 24 + (numWeird * 2)
+                            for j in range(numSpawns):
+                                thisStart = startSpawns + (j * 8)
+                                kasekiNum = int.from_bytes(r[(thisStart + 2):(thisStart + 4)], "little")
+                                used[thisStart + 2] = kasekiNum
+                    for i in range(0, len(r), 2):
+                        if (i in used.keys()):
+                            check = 0
+                            for p in ["Head", "Body", "Arms", "Legs"]:
+                                if (used[i] in fossilTable[p]):
+                                    old = fossilTable[p].index(used[i])
+                                    new = fossilTable[p][vivos[old]]
+                                    f.write(new.to_bytes(2, "little"))
+                                    check = 1
+                                    break
+                            if (check == 0):
+                               f.write(r[i:(i + 2)]) 
                         else:
-                            f.write(r[val:(val + 8)])
-                            if (mapN == "0037"):
-                                f.write((0x6F).to_bytes(4, "little"))
-                                f.write((3).to_bytes(4, "little"))
-                            else:
-                                f.write(r[(val + 8):(val + 16)])
-                            f.write(r[(val + 16):(val + 0x2C)])
-                            numSpawns = int.from_bytes(r[(val + 0x28):(val + 0x2C)], "little")
-                            point3 = int.from_bytes(r[(val + 0x2C):(val + 0x30)], "little")
-                            f.write(r[(val + 0x2C):(val + point3 + (numSpawns * 4))])
-                            for i in range(numSpawns):
-                                point4 = int.from_bytes(r[(val + point3 + (i * 4)):(val + point3 + (i * 4) + 4)], "little")
-                                vivoNum = int.from_bytes(r[(val + point4):(val + point4 + 4)], "little")
-                                if (mapN == "0037"):
-                                    f.write((29).to_bytes(4, "little"))
-                                else:
-                                    f.write(vivos[vivoNum].to_bytes(4, "little"))
-                                f.write(r[(val + point4 + 4):(val + point4 + 32)])
-                                if (i == (numSpawns - 1)) and ((val + point4 + 32) < realP[realP.index(val) + 1]):
-                                    f.write(r[(val + point4 + 32):realP[realP.index(val) + 1]])
+                            f.write(r[i:(i + 2)])
                     f.close()
                     subprocess.run([ "fftool.exe", "compress", "NDS_UNPACK/data/map/m/bin/" + mapN, "-i", "0.bin", "-o",
                         "NDS_UNPACK/data/map/m/" + mapN ])
         shutil.rmtree("NDS_UNPACK/data/map/m/bin/")
+        
+        subprocess.run([ "fftool.exe", "NDS_UNPACK/data/etc/donate_kaseki_defs" ])
+        f = open("NDS_UNPACK/data/etc/bin/donate_kaseki_defs/0.bin", "rb")
+        r = f.read()
+        f.close()
+        f = open("NDS_UNPACK/data/etc/bin/donate_kaseki_defs/0.bin", "wb")
+        f.close()
+        f = open("NDS_UNPACK/data/etc/bin/donate_kaseki_defs/0.bin", "ab")
+        f.write(r[0:0x18])
+        for i in range(0x18, len(r), 8):
+            f.write(r[i:(i + 4)])
+            check = 0
+            curr = int.from_bytes(r[(i + 4):(i + 6)], "little")
+            for p in ["Head", "Body", "Arms", "Legs"]:
+                if (curr in fossilTable[p]):
+                    old = fossilTable[p].index(curr)
+                    new = fossilTable[p][vivos[old]]
+                    f.write(new.to_bytes(2, "little"))
+                    check = 1
+                    break
+            if (check == 0):
+               f.write(r[(i + 4):(i + 6)])
+            f.write(r[(i + 6):(i + 8)])
+        f.close()
+        subprocess.run([ "fftool.exe", "compress", "NDS_UNPACK/data/etc/bin/donate_kaseki_defs/", "-i", "0.bin", "-o",
+            "NDS_UNPACK/data/etc/donate_kaseki_defs/" ])
+        shutil.rmtree("NDS_UNPACK/data/etc/bin/")
+        
     
     if ((res["team"] == "Yes") or (levelR != 0)):
-        subprocess.run([ "fftool.exe", "NDS_UNPACK/data/battle" ])
-        for root, dirs, files in os.walk("NDS_UNPACK/data/battle/bin"):
+        subprocess.run([ "fftool.exe", "NDS_UNPACK/data/battle_param" ])
+        for root, dirs, files in os.walk("NDS_UNPACK/data/battle_param/bin"):
             for file in files:
                 if (file == "0.bin"):
                     f = open(os.path.join(root, file), "rb")
                     r = f.read()
                     f.close()
-                    if (len(r) > 0x94):
+                    if (len(r) > 0x46):
                         f = open(os.path.join(root, file), "wb")
                         f.close()
                         f = open(os.path.join(root, file), "ab")
                         mapN = os.path.join(root, file).split("\\")[-2]
-                        numVivos = r[0x5C]
-                        f.write(r[0:0x94])
+                        shift = full[0x38] + 2 - 0x46
+                        numVivos = r[0x58 + shift]
+                        f.write(r[0:(0x70 + shift)])
                         for i in range(numVivos):
-                            vivoNum = int.from_bytes(r[(0x94 + (i * 12)):(0x94 + (i * 12) + 4)], "little")
-                            if ((vivoNum in list(range(1, 101))) and (res["team"] == "Yes")):
-                                f.write(random.randint(1, 100).to_bytes(4, "little"))
+                            vivoNum = int.from_bytes(r[(0x70 + shift + (i * 12)):(0x70 + shift + (i * 12) + 2)], "little")
+                            if ((vivoNum in list(range(1, 150))) and (res["team"] == "Yes")):
+                                if (res["include"] == "Yes"):
+                                    f.write(random.randint(1, 149).to_bytes(2, "little"))
+                                else:
+                                    newVivo = random.randint(1, 149)
+                                    while newVivo in broken:
+                                        newVivo = random.randint(1, 149)
+                                    f.write(newVivo.to_bytes(2, "little"))
                             else:
-                                f.write(r[(0x94 + (i * 12)):(0x94 + (i * 12) + 4)])
-                            if ((levelR != 0) and (mapN != "0023")):
-                                oldLevel = int.from_bytes(r[(0x94 + (i * 12) + 4):(0x94 + (i * 12) + 8)], "little")
-                                newLevel = max(1, min(oldLevel + levelR, 12))
+                                f.write(r[(0x70 + shift + (i * 12)):(0x70 + shift + (i * 12) + 2)])
+                            if (levelR != 0):
+                                oldLevel = int.from_bytes(r[(0x70 + shift + (i * 12) + 2):(0x70 + shift + (i * 12) + 6)], "little")
+                                newLevel = max(1, min(oldLevel + levelR, 20))
                                 f.write(newLevel.to_bytes(4, "little"))
                             else:
-                                f.write(r[(0x94 + (i * 12) + 4):(0x94 + (i * 12) + 8)])
-                            f.write(r[(0x94 + (i * 12) + 8):(0x94 + (i * 12) + 12)])
-                        f.write(r[(0x94 + (numVivos * 12)):(0x94 + (numVivos * 12) + (numVivos * 8))])
-                        if False: # ((levelR != 0) and (mapN != "0023")):
-                            moveMap = [ 0, 1, 1, 2, 2, 3, 3, 4, 4, 4, 4, 4, 4 ]
-                            for i in range(numVivos):
-                                oldLevel = int.from_bytes(r[(0x94 + (i * 12) + 4):(0x94 + (i * 12) + 8)], "little")
-                                newLevel = max(1, min(oldLevel + levelR, 12))
-                                f.write(moveMap[newLevel].to_bytes(4, "little"))
-                        else:
-                            f.write(r[(0x94 + (numVivos * 12) + (numVivos * 8)):(0x94 + (numVivos * 12) + (numVivos * 12))])
-                        f.write(r[(0x94 + (numVivos * 12) + (numVivos * 12)):])
+                                f.write(r[(0x70 + shift + (i * 12) + 2):(0x70 + shift + (i * 12) + 6)])
+                            f.write(r[(0x70 + shift + (i * 12) + 6):(0x70 + shift + (i * 12) + 12)])
+                        f.write(r[(0x46 + shift + (numVivos * 12)):])
                         f.close()
-                        subprocess.run([ "fftool.exe", "compress", "NDS_UNPACK/data/battle/bin/" + mapN, "-i", "0.bin", "-o",
-                            "NDS_UNPACK/data/battle/" + mapN ])
-        shutil.rmtree("NDS_UNPACK/data/battle/bin/")
+                        subprocess.run([ "fftool.exe", "compress", "NDS_UNPACK/data/battle_param/bin/" + mapN, "-i", "0.bin", "-o",
+                            "NDS_UNPACK/data/battle_param/" + mapN ])
+        shutil.rmtree("NDS_UNPACK/data/battle_param/bin/")
         
     subprocess.run([ "dslazy.bat", "PACK", "out.nds" ])
     subprocess.run([ "xdelta3-3.0.11-x86_64.exe", "-e", "-f", "-s", sys.argv[1], "out.nds", "out.xdelta" ])
